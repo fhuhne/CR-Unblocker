1b2a611c82b32c29735a0440fb31023764661c9d
Blightwidow <theo@dammaretz.fr>
2017-05-17 20:50:00 +0200

    Cleanup and commenting

---
+++ src/background_script.js
@@ -0,0 +1,118 @@
+/* global fetch, chrome */
+var browser = browser || chrome;
+
+const MAINSERVER = 'https://cr.onestay.moe/getid';
+const BACKUPSERVER = 'https://crunchy.rubbix.net/';
+
+/**
+ * Main function fetching and setting the US based cookies
+ * @param {String} extension extension of domain
+ */
+function localizeToUs(extension) {
+	console.log('Setting cookie...');
+	console.log('Trying to fetch main server...');
+	fetchServer(MAINSERVER)
+		.then(sessionId => updateCookies(extension, sessionId))
+		.catch((e) => {
+			console.log(`Got error ${e}. Trying backup server...`);
+			fetchServer(BACKUPSERVER)
+				.then(sessionId => updateCookies(extension, sessionId))
+				.catch((_e) => {
+					notifyUser(`Main server and backup server couldn't get a session id`);
+					console.log(_e);
+				});
+		});
+}
+
+/**
+ * Fetch a session ID from a server
+ * @param  {String} uri URL of the backend server
+ * @return {Promise}     A promise resolving to the the session ID (string)
+ */
+function fetchServer(uri) {
+	return new Promise((resolve, reject) => {
+		fetch(uri)
+			.then((res) => res.json())
+			.then((res) => {
+				if (!res.ok) {
+					reject(new Error(res.error));
+				} else {
+					resolve(res.sessionId);
+				}
+			})
+			.catch((e) => reject(e));
+	});
+}
+
+/**
+ * Update the cookies to the new values
+ * Nested callbacks for Edge compatibility
+ * @param {String} extension hostname extension
+ * @param {String} sessionId  New session ID
+ */
+function updateCookies(extension, sessionId) {
+	console.log('got session id. Setting cookie.');
+	browser.cookies.set({
+		url: `http://crunchyroll${extension}`,
+		name: 'sess_id',
+		value: sessionId,
+		domain: `crunchyroll${extension}`,
+		httpOnly: true
+	}, () => {
+		browser.cookies.set({
+			url: `http://crunchyroll${extension}`,
+			name: 'c_locale',
+			value: 'enUS',
+			domain: `crunchyroll${extension}`,
+			httpOnly: true
+		}, postSetCookie);
+	});
+}
+
+/**
+ * Function called after the cookies are set
+ * Reload the current tab to take the changes into account
+ */
+function postSetCookie() {
+	console.log('Done!');
+	browser.tabs.query({
+		currentWindow: true,
+		active: true
+	}, tabs => {
+		tabs.forEach(tab => {
+			console.log('reload tab via content script');
+			browser.tabs.sendMessage(tab.id, {
+				msg: 'reload'
+			});
+		});
+	});
+}
+
+/**
+ * Emit a notification to the user with an error message
+ * @param  {String} msg Error message
+ */
+function notifyUser(msg) {
+	browser.notifications.create({
+		type: 'basic',
+		iconUrl: 'icons/Crunchyroll-128.png',
+		title: 'CR-Unblocker has encountered an error',
+		message: msg
+	});
+}
+
+/**
+ * Open a new CR tab when the button is pressed
+ */
+browser.browserAction.onClicked.addListener(() => {
+	browser.tabs.create({
+		url: 'http://crunchyroll.com/videos/anime/'
+	});
+});
+
+/**
+ *  Set a cookie with the extension transmitted from the content script
+ */
+browser.runtime.onMessage.addListener((message) => {
+	localizeToUs(message.msg);
+});
---
+++ src/content.js
@@ -0,0 +1,36 @@
+/* global chrome, window, document */
+var browser = browser || chrome;
+
+// this extension was heavily inspired by https://github.com/jerryteps/Crunchyroll-Unblocker
+
+/**
+ * Check if the current CR page is located in the US
+ * @return {Boolean} true if currently in the US
+ */
+function isUs() {
+	let usRegExp = new RegExp('United States of America');
+	let location = document.getElementById('footer_country_flag');
+	return !!usRegExp.test(location.alt);
+}
+
+/**
+ * This function is called everytime the user visit a crunchyroll page
+ * It will ask the background script to get a new cookie if it is not located
+ * in the US
+ */
+if (!isUs()) {
+	let hostname = window.location.hostname;
+	browser.runtime.sendMessage({
+		msg: hostname.slice(hostname.indexOf('crunchyroll.') + 11, hostname.length)
+	});
+}
+
+/**
+ * Reload the page when asked by the background script
+ */
+browser.runtime.onMessage.addListener((message) => {
+	console.log('reloading');
+	if (message.msg === 'reload') {
+		location.reload(true);
+	}
+});
