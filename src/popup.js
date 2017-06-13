29e570d7b1f3a0efad846787e9074bcda63117d4
Blightwidow <theo@dammaretz.fr>
2017-06-13 23:25:49 +0300

    Add notification for data deletion

--- src/popup.js
+++ src/popup.js
@@ -12,7 +12,7 @@ document.getElementById('open-cr').addEventListener('click', () => {
  * Toggle the credential saving mechanism on checkbox toggle
  */
 document.getElementById('save-login').addEventListener('change', (ev) => {
-	browser.storage.local.set({ saveLogin: ev.target.checked });
+	browser.storage.local.set({	saveLogin: ev.target.checked });
 	if (!ev.target.checked) {
 		browser.storage.local.remove(['loginData', 'login']);
 	}
@@ -30,4 +30,12 @@ browser.storage.local.get({ saveLogin: false }, (item) => {
  */
 document.getElementById('delete-data').addEventListener('click', () => {
 	browser.storage.local.remove(['loginData', 'login']);
+	let footer = document.getElementById('footer');
+	let previousText = footer.children[0].innerText;
+	footer.children[0].innerText = 'You data were deleted';
+	footer.classList.add('blink');
+	window.setTimeout(() => {
+		footer.classList.remove('blink');
+		footer.children[0].innerText = previousText;
+	}, 3000);
 });
