fce2cd9d9900423d2e0638c143c65666c1dd470a
Erik Tauchert <erik@tauchert.de>
2017-07-08 17:04:15 +0200

    Circumvent manga API not working properly with HTTPS

--- src/background_script.js
+++ src/background_script.js
@@ -1,7 +1,7 @@
 /* global fetch, chrome, decrypt */
 var browser = browser || chrome;
 
-const API_BASE = 'https://api-manga.crunchyroll.com/cr_start_session?device_id=a&api_ver=1.0';
+const API_BASE = 'http://api-manga.crunchyroll.com/cr_start_session?device_id=a&api_ver=1.0';
 const SERVERS = [
 	`${API_BASE}&device_type=com.crunchyroll.manga.android&access_token=FLpcfZH4CbW4muO`,
 	`${API_BASE}&device_type=com.crunchyroll.iphone&access_token=QWjz212GspMHH9h`,
